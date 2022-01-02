2aad0364a6a1f874d01355578ef56282124af165
Bolouk <amirhosseinbolouk@gmail.com>
2022-01-02 18:30:41 +0330

    Added menu_items to models.py

--- models.py
+++ models.py
@@ -20,3 +20,22 @@ class Manager:
                     "INSERT INTO manager(first_name, last_name, phone_number, email, password) VALUES (%s,%s,%s,%s,%s);"
                     , (self.first_name, self.last_name, self.phone_number, self.email, self.password))
         conn.close()
+
+
+class MenuItem:
+    def __init__(self, name, price, discount, category_id, manager_id) -> None:
+        self.name = name
+        self.price = price
+        self.discount = discount
+        self.category_id = category_id
+        self.manager_id = manager_id
+
+    def add_to_database(self):
+        conn = psycopg2.connect(dbname="pwqucdjl", user="pwqucdjl", password="Q4RNLRzY-lbffdzIJ7hTgxSC2yg7hQ9x",
+                                host='john.db.elephantsql.com', port='5432')
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    "INSERT INTO menu_item(name, price, discount, category_id, manager_id) VALUES (%s,%s,%s,%s,%s);"
+                    , (self.name, self.price, self.discount, self.category_id, self.manager_id))
+        conn.close()

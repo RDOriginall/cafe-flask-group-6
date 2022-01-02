32df5c8df4bdc1166ed35dcc672e5bad9baaf903
Bolouk <amirhosseinbolouk@gmail.com>
2022-01-02 14:44:38 +0330

    commit

---
+++ views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+
+
+def menu_items():
+    # How to get menu from database
+    menu = [
+        {'name': 'Tea', 'price': '10', 'category': 'breakfast'},
+        {'name': 'Cafe', 'price': '20', 'category': 'brunch'},
+        {'name': 'Pizza', 'price': '85', 'category': 'dinner'},
+        {'name': 'macarooni', 'price': '25', 'category': 'dinner'},
+        {'name': 'Chicken noodle', 'price': '35', 'category': 'lunch'},
+        {'name': 'Pizza Pepperooni', 'price': '65', 'category': 'dinner'}
+    ]
+    return render_template('menu.html', menu=menu)

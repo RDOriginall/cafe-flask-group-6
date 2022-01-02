f6e916fcbb9e0646fc7649b218847c7a6a431c5b
Bolouk <amirhosseinbolouk@gmail.com>
2022-01-02 15:02:07 +0330

    commit files for start

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask import Flask
 from views import *
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pwqucdjl:Q4RNLRzY-lbffdzIJ7hTgxSC2yg7hQ9x@john.db.elephantsql.com/pwqucdjl'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# db = SQLAlchemy(app)
+
 
 app.add_url_rule('/menu/', 'menu', menu_items)
 
 if __name__ == '__main__':
     app.run()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pwqucdjl:Q4RNLRzY-lbffdzIJ7hTgxSC2yg7hQ9x@john.db.elephantsql.com/pwqucdjl'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# db = SQLAlchemy(app)

bb4c1f29ac7194abe103e64f17cf25ba418c30b6
Bolouk <amirhosseinbolouk@gmail.com>
2022-01-02 19:08:04 +0330

    added self.id to models

--- models.py
+++ models.py
@@ -19,6 +19,9 @@ class Manager:
                 curs.execute(
                     "INSERT INTO manager(first_name, last_name, phone_number, email, password) VALUES (%s,%s,%s,%s,%s);"
                     , (self.first_name, self.last_name, self.phone_number, self.email, self.password))
+                # finding id of instance
+                curs.execute("SELECT * FROM manager;")
+                self.id = curs.fetchall()[-1][0]
         conn.close()
         # logging
 
@@ -30,6 +33,10 @@ class MenuItem:
         self.discount = discount
         self.category_id = category_id
         self.manager_id = manager_id
+        self.set_id()
+
+    def set_id(self):
+        self.id = ''
 
     def add_to_database(self):
         conn = psycopg2.connect(dbname="pwqucdjl", user="pwqucdjl", password="Q4RNLRzY-lbffdzIJ7hTgxSC2yg7hQ9x",
@@ -39,5 +46,19 @@ class MenuItem:
                 curs.execute(
                     "INSERT INTO menu_item(name, price, discount, category_id, manager_id) VALUES (%s,%s,%s,%s,%s);"
                     , (self.name, self.price, self.discount, self.category_id, self.manager_id))
+                # finding id of instance
+                curs.execute("SELECT * FROM menu_item;")
+                self.id = curs.fetchall()[-1][0]
+        conn.close()
+        # logging
+
+    def update_item(self):
+        conn = psycopg2.connect(dbname="pwqucdjl", user="pwqucdjl", password="Q4RNLRzY-lbffdzIJ7hTgxSC2yg7hQ9x",
+                                host='john.db.elephantsql.com', port='5432')
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    "UPDATE menu_item SET name = %s, price = %s, discount = %s, category_id = %s, manager_id = %s) WHERE menu_item.id = %s;"
+                    , (self.name, self.price, self.discount, self.category_id, self.manager_id, self.id))
         conn.close()
         # logging

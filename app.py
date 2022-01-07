5a5e8f1f6b4d5cc9e24d3130d0c537e38253f1b9
rd-originall <r.d.originall15@gmail.com>
2022-01-07 21:03:02 +0330

    app.py | features added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.add_url_rule('/manager/', 'add_manager', add_manager, methods=['GET', 'POST'
 app.add_url_rule('/menu/', 'menu', menu_items)
 app.add_url_rule('/add_menu_item/', 'add_menu_item', add_menu_items, methods=['GET', 'POST'])
 app.add_url_rule('/cashier/dashboard', 'dashboard', dashboard)
+app.add_url_rule('/cashier/login', 'add_manager', add_manager, methods=['POST'])
 
 
 if __name__ == '__main__':

d08ad8a2b2b10ec2966ba0f4b20539fc1202ec3f
rd-originall <r.d.originall15@gmail.com>
2022-01-15 04:43:22 +0330

    features added | model-manager.py

--- model-manager.py
+++ model-manager.py
@@ -3,7 +3,11 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description="Press -n to start!")
-parser.add_argument("-n", "--name", nargs="+", required=True, help="Insert -n or --name to write your name!")
+parser.add_argument("-fn", "--fname", nargs="+", required=True, help="Insert -fn or --fname to write your first name!")
+parser.add_argument("-ln", "--lname", nargs="+", required=True, help="Insert -ln or --lname to write your last name!")
+parser.add_argument("-ph", "--phone", nargs="+", required=True, help="Insert -ph or --phone to write your phone number!")
+parser.add_argument("-e", "--email", nargs="+", required=True, help="Insert -e or --email to write your email address!")
+parser.add_argument("-p", "--password", nargs="+", required=True, help="Insert -p or --password to write your password!")
 
 args = parser.parse_args()
 

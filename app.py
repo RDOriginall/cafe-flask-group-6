af0e980d1359fa9beffe7c2bb5c51c9dc9554cd7
rd-originall <r.d.originall15@gmail.com>
2022-01-16 19:34:16 +0330

    bugfixed | app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.add_url_rule('/menu/', 'menu', menu_items)
 app.add_url_rule('/add_menu_item/', 'add_menu_item', add_menu_items, methods=['GET', 'POST'])
 app.add_url_rule('/cashier/dashboard', 'dashboard', dashboard)
 app.add_url_rule('/cashier/sign_in', 'add_manager', add_manager, methods=['POST'])
-app.add_url_rule('/cashier/login', 'manager_login', manager_login, methods=['POST'])
+app.add_url_rule('/cashier/login', 'manager_login', manager_login, methods=['GET', 'POST'])
 
 
 if __name__ == '__main__':

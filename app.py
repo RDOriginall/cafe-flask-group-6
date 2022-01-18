16e38da8361e34c0f667a04f8103bf1d3e711411
Bolouk <amirhosseinbolouk@gmail.com>
2022-01-18 17:41:10 +0330

    Added route for cashier menu_items

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
 app.add_url_rule('/menu/', 'menu', menu_items)
 app.add_url_rule('/add_menu_item/', 'add_menu_item', add_menu_items, methods=['GET', 'POST'])
 app.add_url_rule('/cashier/reciept/<reciept_id>', 'print_reciept', print_reciept, methods=['GET', 'POST'])
-app.add_url_rule('/edit_menu_item/<item_id>', 'edit_menu_item', edit_menu_item, methods=['GET', 'POST'])
-app.add_url_rule('/edit_menu_item/', 'edit_menu_item', edit_menu_item, defaults={'item_id':1}, methods=['GET', 'POST'])
+# app.add_url_rule('/edit_menu_item/<item_id>', 'edit_menu_item', edit_menu_item, methods=['GET', 'POST'])
+# app.add_url_rule('/edit_menu_item/', 'edit_menu_item', edit_menu_item, defaults={'item_id':1}, methods=['GET', 'POST'])
 app.add_url_rule('/cashier/dashboard', 'dashboard', dashboard)
 app.add_url_rule('/cashier/sign_in', 'add_manager', add_manager, methods=['POST'])
 app.add_url_rule('/delete_menu_item/', 'delete_menu_item', delete_menu_item, methods=['GET', 'POST'])
@@ -18,7 +18,8 @@ app.add_url_rule('/cashier/login', 'manager_login', manager_login, methods=['GET
 app.add_url_rule('/comment/', 'comment', comment, methods=['POST'])
 app.add_url_rule('/add_order/', 'add_order', add_order, methods=['POST'])
 app.add_url_rule('/get_tables/', 'get_tables', get_tables, methods=['GET'])
-
+app.add_url_rule('/cashier/menu/', 'cashier_menu', cashier_menu, methods=['GET'])
+app.add_url_rule('/edit_menu_item/', 'edit_menu_item', edit_menu_item, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
